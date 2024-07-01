e827d69c3e3ea968d192d47e171c4fc8da83a180
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2024-07-01 15:57:39 -0500

    Fix artwork caching bug

--- artgalleryclient/src/main/kotlin/edu/gvsu/art/client/repository/Caching.kt
+++ artgalleryclient/src/main/kotlin/edu/gvsu/art/client/repository/Caching.kt
@@ -4,11 +4,19 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
+/**
+ * Cache time occurred less than a day ago
+ */
 internal fun isFreshCache(cacheDateTime: String?): Boolean {
-    val today = LocalDateTime.now(ZoneId.of("UTC"))
-    val cacheTime =
-        LocalDateTime.parse(cacheDateTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+    if (cacheDateTime.isNullOrBlank()) {
+        return false
+    }
 
-    // cache time occurred less than a day ago
-    return cacheTime < today.plusDays(1)
+    val now = LocalDateTime.now(ZoneId.of("UTC"))
+    val cacheTime =
+        LocalDateTime.parse(cacheDateTime, CACHE_TIME_FORMAT)
+
+    return cacheTime.plusDays(1) > now
 }
+
+internal val CACHE_TIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
--- artgalleryclient/src/test/kotlin/edu/gvsu/art/client/repository/CachingTest.kt
+++ artgalleryclient/src/test/kotlin/edu/gvsu/art/client/repository/CachingTest.kt
@@ -1,5 +1,27 @@
 package edu.gvsu.art.client.repository
 
-import org.junit.Assert.*
+import junit.framework.TestCase.assertTrue
+import org.junit.Test
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.test.assertFalse
 
-class CachingTest
+class CachingTest {
+    @Test
+    fun isFreshCache_nullValue() {
+        assertFalse(isFreshCache(cacheDateTime = null))
+    }
+
+    @Test
+    fun isFreshCache_oldDate() {
+        assertFalse(isFreshCache(cacheDateTime = "2024-06-11 21:56:32"))
+    }
+
+    @Test
+    fun isFreshCache_validDate() {
+        val now = LocalDateTime.now().minusHours(1)
+
+        assertTrue(isFreshCache(cacheDateTime = now.format(CACHE_TIME_FORMAT)))
+    }
+}
