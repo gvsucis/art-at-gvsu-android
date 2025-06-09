437c0154df43ab373c0430274ceb01fa430e487a
Josiah Campbell <9521010+jocmp@users.noreply.github.com>
2025-06-09 16:58:57 -0500

    wip

--- app/src/main/java/edu/gvsu/art/gallery/ui/search/VisionSearchResultsViewModel.kt
+++ app/src/main/java/edu/gvsu/art/gallery/ui/search/VisionSearchResultsViewModel.kt
@@ -1,6 +1,7 @@
 package edu.gvsu.art.gallery.ui.search
 
 import androidx.core.net.toUri
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.navigation.toRoute
@@ -11,6 +12,14 @@ import edu.gvsu.art.gallery.Route
 class VisionSearchResultsViewModel(
     handle: SavedStateHandle,
     private val client: VisionSearchClient
-) : ViewModel() {
+) : AndroidViewModel() {
     val uri = handle.toRoute<Route.VisionSearchResults>().imageUri.toUri()
+
+    fun search() {
+//        val inputStream = context.contentResolver.openInputStream(uri)
+//        val imageData =
+//            inputStream?.readBytes() ?: throw IllegalArgumentException("Cannot read image data")
+//        val requestBody = imageData.toRequestBody("image/jpeg".toMediaTypeOrNull())
+//        val multipart = MultipartBody.Part.createFormData("image", "image.jpg", requestBody)
+    }
 }
